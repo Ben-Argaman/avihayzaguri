9857eda47d36185e0fbca08543deb94e3be1fb5f
Ben-Argaman <122999427+Ben-Argaman@users.noreply.github.com>
2023-01-29 16:53:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
+
+module.exports = {
+   experimental: {
     appDir: false,
   },
-};
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
 }
-module.exports = nextConfig;
