16f760dfb66ed65182edd7ee46ecc8048d41f5c8
Ben-Argaman <122999427+Ben-Argaman@users.noreply.github.com>
2023-01-29 16:47:45 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
-     quotes: [2, "single", { "avoidEscape": true }],
+     quotes: [0, "double"],
     'react/no-unescaped-entities': 0,
     'eslintreact/no-danger': 0,
     'react/jsx-max-props-per-line': 0,
